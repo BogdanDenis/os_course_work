f3d92ee33a81ff1ccc65444cb01382eabd9d4628
BogdanDenis <denys.bohdan@nure.ua>
2017-11-08 19:41:33 +0200

    removed client code from server

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -7,11 +7,10 @@
 
 using namespace std;
 
+int socket_desc, client_sock;
+struct sockaddr_in server, client;
 
-int main () {
-		int socket_desc, client_sock, c, read_size;
-		struct sockaddr_in server, client;
-		char client_message[4200];
+void openServerSocket() {
 
 		//Create socket
 		socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -23,51 +22,68 @@ int main () {
 		//Prepare the sockaddr_in struct
 		server.sin_family = AF_INET;
 		server.sin_addr.s_addr = INADDR_ANY;
-		server.sin_port = htons(54002);
+		server.sin_port = htons(54000);
 
 		//Bind
 		if (bind(socket_desc, (struct sockaddr*)&server, sizeof(server)) < 0) {
 				perror("bind failed. Error");
-				return 1;
+				exit(1);
 		}
 		puts("bind done");
-		while (true) {
-				//Listen
-				listen(socket_desc, 1);
+}
 
-				//Accept incoming connection
-				puts("waiting for incoming connections...");
-				c = sizeof(struct sockaddr_in);
+void acceptClient() {
+		puts("waiting for incoming connections...");
+				int c = sizeof(struct sockaddr_in);
 
 				//Accept connection from an incoming client
 				client_sock = accept(socket_desc, (struct sockaddr*)&client, (socklen_t*)&c);
 				if (client_sock < 0) {
 						perror("accept failed");
-						return 1;
+						exit(1);
 				}
 				puts("Connection accepted");
+}
+
+void listenToSocket() {
+		listen(socket_desc, 1);
+				char client_message[4200];
+				int read_size = 0;
+				//Accept incoming connection
+				acceptClient();		
 
 				//Receive a message from client
 				puts("Waiting for file transmittion to begin...\n");
-
-
+				string fileName = "";
 				while ((read_size = recv(client_sock, client_message, 4097, 0)) > 0) {
 						printf ("Read %d\n", read_size);
 						//Send the message back
-						printf("Received: %s \n", client_message);
 						string msg = client_message;
 						string reply;
+						int len = 0;
+						///   3   ///
+
 						if (msg.find("<NAME>") != -1) {
+								
+								///   4   ///
+								fileName = (string)msg.substr(6);
+								printf("File name: %s\n", fileName.c_str());
 								reply = "<OK>\0";
 						}
 						else if (msg.find("<LENGTH>") != -1) {
+						
+								///   8   ///
+								len = atoi(msg.substr(8).c_str());
+								printf("Length: %d\n", len);
 								reply = msg;
 						}
 						else if (msg.find("<FILE>") != -1) {
 								printf("File transmittion began\n");
 								reply = "<OK>\0";
 								
-								//
+
+								///   12   ///
+
 								send(client_sock, reply.c_str(), reply.size() + 1, 0);
 								
 								
@@ -75,27 +91,47 @@ int main () {
 								string data = "";
 								printf("Waiting for packs...\n");
 								
+								///   13, 17   ///
 								
-								while ((read_size = recv(client_sock, client_message, 4201, 0)) > 0) {
-										
-										printf("Received\n");
-										msg = client_message;
-										printf("%s \n", client_message);
-										char buff[4096];
-										memset(buff, '\0', 4096);
-										data += (string)client_message;
-										//
-										reply = "<SIZE>" + strlen(client_message);
-												
-										//
-										send(client_sock, reply.c_str(), reply.size() + 1, 0);
-												
-												
-										puts("Waiting for packs...\n");
+								unsigned char binary_data[4096];
+								memset(binary_data, '\0', 4096);
+
+								int received = 0;
+								FILE *file = fopen(fileName.c_str() , "wb");
+								if (!file) {
+								//		ferror(file);
+										printf("Could not create or open file\n");
 								}
+								while ((read_size = recv(client_sock, binary_data, 4096, 0)) > 0) {
+										printf("Received %d bytes\n", read_size);
+										if ((string)((const char*)(binary_data)) == "<END>") {
+												printf("END received\n");
+												reply = "<OK>";
+												send(client_sock, reply.c_str(), reply.size() + 1, 0);
+												break;
+										}
+										else {
+												//printf("Copying %d bytes of data\n", read_size - 1);
+												//memcpy(&file_data[received], binary_data, read_size - 1);
+												//printf("Copied %d bytes of data\n", read_size - 1);
+												fwrite(binary_data, 1, read_size, file);
+												received += read_size - 1;
+												//printf("Received: %d\n", received);
+										}
+										
+										//
+										reply = "<SIZE>" + to_string(read_size);
+												
+										
+										///   16   ///
+
+										send(client_sock, reply.c_str(), reply.size() + 1, 0);
+								}
+								printf("Received %d bytes of data in total\n", received);
+								//FILE *file = fopen(fileName.c_str(), "bw");
+								//fwrite(file_data, sizeof(unsigned char), len, file);
 						}
 
-						//
 						send(client_sock, reply.c_str(), reply.size() + 1, 0);
 						
 						
@@ -112,6 +148,16 @@ int main () {
 				else {
 						puts("Waiting for file transmittion to begin...\n");
 				}
+}
+
+int main () {
+		int c, read_size;
+		
+		
+		openServerSocket();
+
+		while (true) {
+				listenToSocket();
 		}
 		int status = shutdown(socket_desc, SHUT_RDWR);
 		if (!status)
