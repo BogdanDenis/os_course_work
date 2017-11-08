3f610226c759f95b1ef7ce46fc3f669096185956
BogdanDenis <denys.bohdan@nure.ua>
2017-11-08 19:37:17 +0200

    removed server code from client

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -60,10 +60,12 @@ int sockClose(int sock) {
 		return status;
 }
 
+#define PACK_SIZE 4096
+
 int sock;
 
 bool sendData(const char* message, int len) {
-		if (len < 4096) {
+		if (len < PACK_SIZE) {
 				if (send(sock, message, len, 0) < 0) {
 						puts("Send failed");
 						return 1;
@@ -71,15 +73,41 @@ bool sendData(const char* message, int len) {
 		}
 }
 
-string receiveReply() {
-		char reply[4096];
-		memset(reply, '\0', 4096);
+bool sendData(const unsigned char* message, int len) {
+		if (len < PACK_SIZE) {
+				if (send(sock, message, len, 0) < 0) {
+						printf("Send failed\n");
+						return 0;
+				}
+		}
+}
 
-		if (recv(sock, reply, 4096, 0) < 0) {
+string receiveReply() {
+		char reply[PACK_SIZE];
+		memset(reply, '\0', PACK_SIZE);
+
+		if (recv(sock, reply, PACK_SIZE, 0) < 0) {
 				printf("recv failed");
 				return "";
 		}
 		return (string)(reply);
+}
+
+
+unsigned char* getFileData(string filePath, unsigned long &fileLen) {
+		FILE *file = fopen(filePath.c_str(), "rb");
+		fseek(file, 0 , SEEK_END);
+		fileLen = ftell(file);
+		unsigned char *file_data;
+		rewind(file);
+		file_data = new unsigned char[fileLen * sizeof(char)];
+		if (!file_data) {
+				printf("Memory error");
+				return NULL;
+		}
+		fread(file_data, fileLen, 1, file);
+		fclose(file);
+		return file_data;
 }
 
 int sendFile() {
@@ -88,94 +116,98 @@ int sendFile() {
 		printf("Input file path: ");
 		scanf("%s", tmp);
 		filePath = tmp;
-
-		FILE *file = fopen(filePath.c_str(), "rb");
-		fseek(file, 0 , SEEK_END);
-		unsigned long fileLen = ftell(file);
-		char *file_data;
-		rewind(file);
-		file_data = new char[fileLen * sizeof(char)];
-		if (!file_data) {
-				printf("Memory error");
-				return 1;
-		}
-		int num_read = 0;
-		char s;
-		while ((num_read = fread(&s, 1, 1, file))) {
-				strncat(file_data, &s, 1);
-		}
-		fclose(file);
 		
-
+		unsigned long fileLen;
+		unsigned char* file_data = getFileData(filePath, fileLen);
 
 		string fileName = "<NAME>" + filePath.substr(filePath.find_last_of("/") + 1);
-		//Send file name
+		
+		///   1   ///
+
 		sendData(fileName.c_str(), fileName.size() + 1);
+		
+		///   2   ///
+		
 		string reply = receiveReply(); //should be <OK>
-		printf("Reply: %s\n", reply.c_str());
+		
 		if (reply == "<OK>") {
 				printf("File name successfully sent\n");
 
 				string fileLength = "<LENGTH>" + to_string(fileLen);
-				//Send file length
+
+				///   6   ///
+
 				sendData(fileLength.c_str(), fileLength.size() + 1);
+				
+				///   7   ///
+
 				reply = receiveReply(); // should be <LENGTH>[length]
-				printf("Data: %s, reply: %s\n", fileLength.c_str(), reply.c_str());
+
 				if (reply == fileLength) {
 						printf("File length successfully sent\n");
 						
 						string msg = "<FILE>";
 						
-						//
+						///   10   ///
+						
 						sendData(msg.c_str(), msg.size() + 1);
 						
-						
-						printf("Listening...\n");
-						
-						//
+						///   11   ///
+
 						reply = receiveReply(); //should be <OK>
 						
 						
 						if (reply == "<OK>") {
 								printf("File transmittion began\n");
+
 								int bytes_sent = 0;
 								int len = (int)(fileLen) * sizeof(char);
-								
+								printf("File length: %d bytes\n", len);
+
 								while (bytes_sent != len) {
-										printf("%d bytes sent\n", bytes_sent);
-										char pack[4096];
-										memset(pack, '\0', 4096);
+										char pack[PACK_SIZE];
+										memset(pack, '\0', PACK_SIZE);
 										int currently_sent = 0;
-										if (len - bytes_sent >= 4095) {
-												strncpy(pack, file_data + bytes_sent, 4095);
-												bytes_sent += 4095;
-												currently_sent = 4095;
+										if (len - bytes_sent >= PACK_SIZE - 1) {
+												printf("Packing %d bytes of data\n", PACK_SIZE - 1);
+												memcpy(pack, file_data + bytes_sent, PACK_SIZE - 1);
+												bytes_sent += PACK_SIZE - 1;
+												currently_sent = PACK_SIZE - 1;
 										}
 										else {
-												strncpy(pack, file_data + bytes_sent, len - bytes_sent);
-												bytes_sent += (len - bytes_sent);
+												printf("Packing %d bytes of data\n", len - bytes_sent);
+												memcpy(pack, file_data + bytes_sent, len - bytes_sent);
 												currently_sent = len - bytes_sent;
+												bytes_sent += (len - bytes_sent);
 										}
+										pack[currently_sent] = '\0';
 										
-										//
-										sendData("ASDADSAD\0", 4096);
+										printf("Sending %d bytes of data\n", currently_sent);
 										
-										//
+										///   14   ///
+
+										sendData(pack, currently_sent);
+										///   15   ///
+
 										reply = receiveReply(); // should be <SIZE>[currently_sent]
 
 										if (reply == "<SIZE>" + to_string(currently_sent)) {
-												printf("Pack [%d - %d] successfully sent", bytes_sent - currently_sent, bytes_sent);
+												printf("Pack [%d - %d] successfully sent\n", bytes_sent - currently_sent, bytes_sent);
 										}
 										else {
-												printf ("Pack [%d - %d] was not sent", bytes_sent - currently_sent, bytes_sent);
+												printf ("Pack [%d - %d] was not sent\n", bytes_sent - currently_sent, bytes_sent);
 										}
 								}
-								msg = "<END>";
+								printf("File was sent. Waiting for confirmation\n");
+								msg = "<END>\0";
 								
-								//
+								
+								///   18   ///
+
 								sendData(msg.c_str(), msg.size() + 1);
 								
-								//
+								///   19   ///
+								
 								reply = receiveReply(); //should be <OK>
 								
 								
@@ -192,7 +224,7 @@ int sendFile() {
 int main () {
 		initSock();
 		struct sockaddr_in server;
-		char message[4096], reply[4096];
+		char message[PACK_SIZE], reply[PACK_SIZE];
 
 		//Create socket
 		sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -202,7 +234,7 @@ int main () {
 		puts("Socket created");
 		server.sin_addr.s_addr = inet_addr("127.0.0.1");
 		server.sin_family = AF_INET;
-		server.sin_port = htons(54002);
+		server.sin_port = htons(54000);
 
 		//Connect to remote server
 		if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
